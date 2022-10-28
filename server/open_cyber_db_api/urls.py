f4a8f73129523b24cce7c500bb5a6f5ae4914160
NGABO MUGISHA ROBERT <ngabo.mugisha.bro@gmail.com>
2022-10-28 11:51:24 +0200

    Update urls.py

--- server/open_cyber_db_api/urls.py
+++ server/open_cyber_db_api/urls.py
@@ -51,9 +51,9 @@ urlpatterns = [
 
     # for Heroku
 
-    re_path(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve,
+    #     {'document_root':       settings.MEDIA_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve,
+    #     {'document_root': settings.STATIC_ROOT}),
 
 ]
